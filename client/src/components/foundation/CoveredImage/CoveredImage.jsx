72d053ab522832c2496ef8202b0380e14620e69c
ktknest <katou.kennichi@gmail.com>
2021-05-13 21:41:17 +0900

    画像の読み込み周りをシンプルに

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -1,9 +1,5 @@
 import classNames from 'classnames';
-import sizeOf from 'image-size';
 import React from 'react';
-
-import { useFetch } from '../../../hooks/use_fetch';
-import { fetchBinary } from '../../../utils/fetchers';
 
 /**
  * @typedef {object} Props
@@ -16,15 +12,18 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @type {React.VFC<Props>}
  */
 const CoveredImage = ({ alt, src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
+  const [imageSize, setImageSize] = React.useState({ height: 0, width: 0 });
 
-  const imageSize = React.useMemo(() => {
-    return data !== null ? sizeOf(Buffer.from(data)) : null;
-  }, [data]);
-
-  const blobUrl = React.useMemo(() => {
-    return data !== null ? URL.createObjectURL(new Blob([data])) : null;
-  }, [data]);
+  React.useEffect(() => {
+    const image = new Image();
+    image.onload = () => {
+      setImageSize({
+        height: image.height,
+        width: image.width,
+      });
+    };
+    image.src = src;
+  }, [src]);
 
   const [containerSize, setContainerSize] = React.useState({ height: 0, width: 0 });
   /** @type {React.RefCallback<HTMLDivElement>} */
@@ -34,10 +33,6 @@ const CoveredImage = ({ alt, src }) => {
       width: el?.clientWidth ?? 0,
     });
   }, []);
-
-  if (isLoading || data === null || blobUrl === null) {
-    return null;
-  }
 
   const containerRatio = containerSize.height / containerSize.width;
   const imageRatio = imageSize?.height / imageSize?.width;
@@ -50,7 +45,7 @@ const CoveredImage = ({ alt, src }) => {
           'w-auto h-full': containerRatio > imageRatio,
           'w-full h-auto': containerRatio <= imageRatio,
         })}
-        src={blobUrl}
+        src={src}
       />
     </div>
   );
